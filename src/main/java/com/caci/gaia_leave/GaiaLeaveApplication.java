0685912686ee039c319352ec56f6ab10ded7aa9e
minja <mmiljkovic@finmetrixgroup.com>
2025-02-17 23:54:07 +0100

    17022025 Initial project setup with Maven, added dependencies for Apache Commons Codec and Jackson

---
+++ src/main/java/com/caci/gaia_leave/GaiaLeaveApplication.java
@@ -0,0 +1,13 @@
+package com.caci.gaia_leave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaiaLeaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaiaLeaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caci/gaia_leave/GaiaLeaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caci.gaia_leave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaiaLeaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
