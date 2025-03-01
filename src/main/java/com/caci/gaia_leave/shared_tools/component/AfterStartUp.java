0b29e3110362aacab45be8d9030ec5555971e0c9
minja <mmiljkovic@finmetrixgroup.com>
2025-03-01 18:22:11 +0100

    01032025 v3

--- src/main/java/com/caci/gaia_leave/shared_tools/component/AfterStartUp.java
+++ src/main/java/com/caci/gaia_leave/shared_tools/component/AfterStartUp.java
@@ -1,29 +1,28 @@
 package com.caci.gaia_leave.shared_tools.component;
 
-import com.caci.gaia_leave.administration.model.response.UserResponse;
-import com.caci.gaia_leave.administration.repository.response.UserResponseRepository;
-import com.caci.gaia_leave.administration.service.AuthorizationService;
-import com.caci.gaia_leave.administration.service.RoleService;
-import com.caci.gaia_leave.shared_tools.configuration.AppProperties;
-import com.caci.gaia_leave.shared_tools.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.caci.gaia_leave.shared_tools.helper.AllHelpers.*;
 
 @Component
 @RequiredArgsConstructor
 public class AfterStartUp {
 
-
-    private final JwtService jwtService;
-    private final AuthorizationService authorizationService;
-
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() {
-//        authorizationService.login("system", "3011972Fenix!");
+
+            int year = 2025;
+            List<LocalDate> weekends = getWeekends(year);
+            LocalDate today = getOrthodoxEasterDate(year);
+
+            System.out.println("Easter: " + today);
+
     }
+
 }
--- src/main/java/com/caci/gaia_leave/shared_tools/helper/AllHelpers.java
+++ src/main/java/com/caci/gaia_leave/shared_tools/helper/AllHelpers.java
@@ -2,6 +2,9 @@ package com.caci.gaia_leave.shared_tools.helper;
 
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +16,60 @@ public class AllHelpers {
         return list;
     }
 
+    public static List<LocalDate> getWeekends(int year) {
+        List<LocalDate> weekends = new ArrayList<>();
+        LocalDate date = LocalDate.of(year, 1, 1);
 
+        while (date.getYear() == year) {
+            if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                weekends.add(date);
+            }
+            date = date.plusDays(1);
+        }
+        return weekends;
+    }
+
+    /*
+     * Ova metoda računa datum pravoslavnog Uskrsa koristeći algoritam koji se zasniva na lunarnim ciklusima
+     * i strukturi nedelje. U formuli se koriste sledeći ključni pojmovi:
+     *
+     * Metonov ciklus:
+     * - Period od 19 godina nakon kojeg se približno ponavljaju faze Meseca (lunarne faze).
+     * - Varijabla 'a' predstavlja poziciju godine unutar ovog ciklusa.
+     *
+     * Lunarna faza (Mesec):
+     * - Odnosi se na periodične promene u vidljivosti Meseca.
+     * - U algoritmu se uzimaju u obzir lunarne karakteristike (lunarni mesec) da bi se pravilno odredio datum Uskrsa.
+     *
+     * Ostatak varijabli:
+     * - 'b' i 'c' su ostaci pri deljenju godine sa 4 i 7, što pomaže u određivanju rasporeda dana u nedelji.
+     * - 'd' kombinuje poziciju u Metonovom ciklusu sa dodatnim podešavanjem za lunarne efekte.
+     * - 'e' predstavlja dodatno podešavanje vezano za dane u nedelji.
+     * - 'f' je zbir 'd' i 'e' i predstavlja ukupni pomak dana do Uskrsa.
+     *
+     * Na kraju, datum Uskrsa se računa kao 4 + f, što određuje dan u aprilu kada pada Uskrs.
+     */
+    public static LocalDate getOrthodoxEasterDate(int year) {
+        // 'a': Pozicija godine u Metonovom ciklusu (19-godišnji ciklus koji prati ponavljanje lunarnih faza)
+        int a = year % 19;
+
+        // 'b': Ostatak pri deljenju godine sa 4, pomaže u određivanju dana u nedelji
+        int b = year % 4;
+
+        // 'c': Ostatak pri deljenju godine sa 7, dodatno podešavanje za raspored dana u nedelji
+        int c = year % 7;
+
+        // 'd': Kombinuje poziciju u Metonovom ciklusu sa lunarnim podešavanjem
+        int d = (19 * a + 16) % 30;
+
+        // 'e': Dodatno podešavanje bazirano na vrednostima 'b', 'c' i 'd' za tačan proračun dana u nedelji
+        int e = (2 * b + 4 * c + 6 * d) % 7;
+
+        // 'f': Ukupni pomak dana do Uskrsa (kombinacija lunarne i sedmične komponente)
+        int f = d + e;
+
+        // Vraća datum Uskrsa: aprilski datum određen kao (4 + f) dan u mesecu
+        return LocalDate.of(year, 4, f + 4);
+    }
 
 }
