a391f2a838148641ffbbab65b8c0b32fd7b32aec
minja <mmiljkovic@finmetrixgroup.com>
2025-03-28 22:54:46 +0100

    28032025 v1

--- src/main/java/com/caci/gaia_leave/administration/service/MailService.java
+++ src/main/java/com/caci/gaia_leave/administration/service/MailService.java
@@ -2,12 +2,19 @@ package com.caci.gaia_leave.administration.service;
 
 import com.caci.gaia_leave.shared_tools.configuration.AppProperties;
 import com.caci.gaia_leave.shared_tools.exception.CustomException;
+import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
 
 @Service
 @RequiredArgsConstructor
@@ -15,38 +22,50 @@ public class MailService {
     private final AppProperties appProperties;
     private final JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String body, MultipartFile[] files) {
-
-        if (to == null || to.isEmpty()) {
-            throw new IllegalArgumentException("Sender cannot be null or empty");
-        }
-
-        if (subject == null || subject.isEmpty()) {
-            throw new IllegalArgumentException("Subject cannot be null or empty");
-        }
-
-        if (body == null || body.isEmpty()) {
-            throw new IllegalArgumentException("Body cannot be null or empty");
-        }
-
+    public void sendEmail(String sendTo, String subject, String body, MultipartFile[] files) {
+        String prepareAddresses = sendTo.trim().replaceAll("\\s+", "");
         boolean check = files != null;
+        FileSystemResource fsr = null;
 
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, check);
             helper.setFrom(appProperties.getSendMailFrom());
-            helper.setTo(to);
+            helper.setTo(InternetAddress.parse(prepareAddresses));
             helper.setSubject(subject);
             helper.setText(body, true);
             if (check) {
                 for (MultipartFile file : files) {
                     String fileName = file.getOriginalFilename();
-                    helper.addAttachment(fileName, file);
+                    fsr = new FileSystemResource(fileConverter(file));
+                    helper.addAttachment(fileName, fsr);
                 }
             }
             mailSender.send(message);
+
+            if (fsr != null) {
+                try {
+                    if (fsr.isFile()) Files.delete(fsr.getFile().toPath());
+                } catch (Exception e) {
+                    throw new CustomException("File in attachment is required");
+                }
+            }
+
         } catch (Exception e) {
             throw new CustomException(e.getMessage());
         }
     }
+
+    private File fileConverter(MultipartFile file) {
+        File tempFile = new File(file.getOriginalFilename());
+        try {
+            FileOutputStream fos = new FileOutputStream(tempFile);
+            fos.write(file.getBytes());
+            fos.close();
+            return tempFile;
+        } catch (Exception e) {
+            throw new CustomException(e.getMessage());
+        }
+    }
+
 }
