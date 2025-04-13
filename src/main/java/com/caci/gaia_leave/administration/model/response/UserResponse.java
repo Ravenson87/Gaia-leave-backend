a241e2b7a98b2a797ec26cc5b5e7989cd1cc21ce
minja <mmiljkovic@finmetrixgroup.com>
2025-04-13 18:00:24 +0200

    13042025 v1

--- src/main/java/com/caci/gaia_leave/administration/model/response/UserResponse.java
+++ src/main/java/com/caci/gaia_leave/administration/model/response/UserResponse.java
@@ -162,10 +162,4 @@ public class UserResponse implements Serializable {
     @JoinColumn(name = "user_id", referencedColumnName = "id",
             insertable = false, updatable = false)
     private List<UserDocumentsResponse> userDocuments;
-
-    @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-    @OneToMany(fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_id", referencedColumnName = "id",
-            insertable = false, updatable = false)
-    private List<FreeDaysBookingResponse> freeDaysBooking;
 }
